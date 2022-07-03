1576196593f04423323d0bb406945af719d9a3ad
Oleg Borodkin <oborodkin@gmail.com>
2022-07-03 15:00:03 +0600

    основое задание и балансировщик

---
+++ terraform/outputs.tf
@@ -0,0 +1,6 @@
+output "external_ip_address_app" {
+  value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+}
+output "external_ip_address_app2" {
+  value = yandex_compute_instance.app2.network_interface.0.nat_ip_address
+}
